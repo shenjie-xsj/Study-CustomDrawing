eb2fc70f0b2611536adf1e3dddbb07d76dfe4a9f
EU_ShenJie <shenjie_xsj@yeah.net>
2018-07-26 17:10:18 +0800

    新建项目

---
+++ StudyCustomDrawing/UseLayer/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  UseLayer
+//
+//  Created by EU_ShenJie on 2018/7/26.
+//  Copyright © 2018年 EU_ShenJie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var layerView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        //添加一个CALayer对象
+        let blueLayer : CALayer = CALayer()
+        //设置CALayer对象属性
+        blueLayer.frame = CGRect(x: 50, y: 50, width: 100, height: 100) //设置位置
+        blueLayer.backgroundColor = UIColor.blue.cgColor    //设置背景颜色
+        //添加为子图层
+        layerView.layer.addSublayer(blueLayer)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
