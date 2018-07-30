c1de80f94d50033aa6c33b020ad78d71c7342d71
EU_ShenJie <shenjie_xsj@yeah.net>
2018-07-30 15:39:40 +0800

    完成对于CustomDrawing的学习

--- StudyCustomDrawing/UseLayer/ViewController.swift
+++ StudyCustomDrawing/UseLayer/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , CALayerDelegate {
     @IBOutlet weak var layerView: UIView!
     
     override func viewDidLoad() {
@@ -21,11 +21,25 @@ class ViewController: UIViewController {
         //设置CALayer对象属性
         blueLayer.frame = CGRect(x: 50, y: 50, width: 100, height: 100) //设置位置
         blueLayer.backgroundColor = UIColor.blue.cgColor    //设置背景颜色
+        
+        //设置图层的代理为本UIViewController（需要遵守CALayerDelegate协议）
+        blueLayer.delegate = self
+        //设置点与像素的转换呈现方式（和系统底层保持一致）
+        blueLayer.contentsScale = UIScreen.main.scale
+        
         //添加为子图层
         layerView.layer.addSublayer(blueLayer)
         
+        blueLayer.display()
     }
 
+    //CALayerDelegate协议方法（用于明确如何绘图）
+    func draw(_ layer: CALayer, in ctx: CGContext) {
+        ctx.setLineWidth(10)
+        ctx.setStrokeColor(UIColor.red.cgColor)
+        ctx.strokeEllipse(in: layer.bounds)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
